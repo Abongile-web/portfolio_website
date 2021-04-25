0c92e6b65a241f33bb728ef04f6bbe22ce65148d
Abongile William <abongilewilliam@gmail.com>
2021-04-25 19:59:35 +0200

    nav functional

--- public/app.js
+++ public/app.js
@@ -3,6 +3,18 @@ document.querySelector('#menu').addEventListener('click', () => {
     document.querySelector('.navbar').classList.toggle('navbar_show');
 })
 
+let menu_items = document.querySelectorAll('.navbar ul li');
+for (let i = 0; i < menu_items.length -1; i++) {
+    console.log(menu_items[i]);
+    menu_items[i].addEventListener('click', () => {
+        document.querySelector('.navbar').classList.toggle('navbar_show');
+    })
+}
+
+document.querySelector('.logo').addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('navbar_show');
+})
+
   //Contact form 
 $(document).ready(function () {
   $('.submit').click(function (event) {
@@ -13,7 +25,7 @@ $(document).ready(function () {
       var status = $('.status')
       status.empty()
 
-      if(email.length > 5 && email.includes('@') && email.includes('.')){
+      if(email.length > 1 && email.includes('@') && email.includes('.')){
           status.append('<div>Email is valid</div>')
       }
       else {
@@ -21,7 +33,7 @@ $(document).ready(function () {
           status.append('<div>Email is not valid</div>')
       }
 
-      if(subject.length >= 2) {
+      if(subject.length >= 1) {
           status.append('<div>Subject is valid</div>')
       }
       else {
@@ -29,7 +41,7 @@ $(document).ready(function () {
           status.append('<div>Subject is not valid</div>')
       }
 
-      if(message.length >= 10) {
+      if(message.length >= 1) {
           status.append('<div>Message is valid</div>')
       }
       else {
